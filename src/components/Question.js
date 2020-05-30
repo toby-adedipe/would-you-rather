eddc2e5f615d6f20560caa6068de3eaa1a905081
Adedipe Toby <toby_adedipe@yahoo.com>
2020-05-30 13:22:23 +0100

    wrote logic to check if a user has voted

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,20 +5,24 @@ import voted from './voted.jpg'
 
 class Question extends Component{
     state={
-        value: ''
+        value: '',
+        hasVoted: false
     }
     constructor(props){
         super(props);
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    componentDidMount(){
+        this.hasVoted()
+    }
+
     handleChange(value){
         this.setState(()=>({
             value
         }))
     }
-
-
 
     handleSubmit(e){
         e.preventDefault()
@@ -26,6 +30,21 @@ class Question extends Component{
         const answer = this.state.value
         
         dispatch(handleSaveAnswer({authedUser, id, answer}))
+        this.setState(()=>({
+            hasVoted: true
+        }))
+    }
+
+    hasVoted(){
+        const { id, authedUser, questions } = this.props
+
+        questions[id].firstOption.votes.includes(authedUser) || questions[id].secondOption.votes.includes(authedUser)
+            ? this.setState(()=>({
+                hasVoted: true
+            }))
+            : this.setState(()=>({
+                hasVoted: false
+            }))
     }
 
     render(){
@@ -36,79 +55,85 @@ class Question extends Component{
         let secondOptionVotes = questions[id].secondOption.votes.length
         let firstPercentage = firstOptionVotes === 0 
                                 ? 0
-                                : ((firstOptionVotes/totalVotes)*100)
+                                : Math.trunc((firstOptionVotes/totalVotes)*100)
         let secondPercentage = secondOptionVotes === 0 
                                 ? 0
-                                : ((secondOptionVotes/totalVotes)*100)
+                                : Math.trunc((secondOptionVotes/totalVotes)*100)
 
         return(
             <div>
-                <h3 className="question-header">{users[questions[id].askedBy].name} wants to know</h3>
-                <div className="question">
-                    <div className="question-image-section">
-                        <img
-                            src={users[questions[id].askedBy].avatar}
-                            alt={`${users[questions[id].askedBy].name}'s avatar`}
-                        />
-                    </div>
-                    <div className="question-quest-section">
-                            <h4>Would you Rather?</h4>
-                            <form onSubmit={this.handleSubmit}>
-                                <label className="question-options">
-                                    <input
-                                        type='radio'
-                                        name='options'
-                                        value='firstOption'
-                                        onChange={(e)=>this.handleChange(e.target.value)}
-                                    />
-                                    {questions[id].firstOption.text}
-                                </label>
-                                <label className="question-options">
-                                    <input
-                                        type='radio'
-                                        name='options'
-                                        value='secondOption'
-                                        onChange={(e)=>this.handleChange(e.target.value)}
-                                    />
-                                    {questions[id].secondOption.text}
-                                </label>
-                                <button type="submit" className="primary-btn" disabled={this.state.value.length ===0}>Submit</button>
-                            </form>
-                    </div>
-                </div>
-
-                <div>
+                { !this.state.hasVoted && (
                     <div>
-                        <h3 className='result-header'>Asked By {users[questions[id].askedBy].name}</h3>
-                        <div className='result-div'>
-                            <h4 className='result-div-header'>Results</h4>
-                            <div className='result'>
-                                <div className='result-image-section'>
-                                    <img
-                                        src={users[questions[id].askedBy].avatar}
-                                        alt={`${users[questions[id].askedBy].name}'s avatar`}
-                                    />
-                                </div>
-                                <div className='result-quest-section'>
-                                    <div className="firstOption">
-                                        <img
-                                            src={voted}
-                                            alt="voted-icon"
-                                            className='voted-icon'
-                                        />
-                                        <p>{questions[id].firstOption.text}</p>
-                                        <p><span>{`${firstPercentage}%`}</span> <span>{`${firstOptionVotes} of ${totalVotes} total votes`}</span></p>
-                                    </div>
-                                    <div className="secondOption">
-                                        <p>{questions[id].secondOption.text}</p>
-                                        <p><span>{`${secondPercentage}%`}</span> <span>{`${secondOptionVotes} of ${totalVotes} total votes`}</span></p>
-                                    </div>
-                                </div>
+                        <h3 className="question-header">{users[questions[id].askedBy].name} wants to know</h3>
+                        <div className="question">
+                            <div className="question-image-section">
+                                <img
+                                    src={users[questions[id].askedBy].avatar}
+                                    alt={`${users[questions[id].askedBy].name}'s avatar`}
+                                />
                             </div>
-                            
+                            <div className="question-quest-section">
+                                    <h4>Would you Rather?</h4>
+                                    <form onSubmit={this.handleSubmit}>
+                                        <label className="question-options">
+                                            <input
+                                                type='radio'
+                                                name='options'
+                                                value='firstOption'
+                                                onChange={(e)=>this.handleChange(e.target.value)}
+                                            />
+                                            {questions[id].firstOption.text}
+                                        </label>
+                                        <label className="question-options">
+                                            <input
+                                                type='radio'
+                                                name='options'
+                                                value='secondOption'
+                                                onChange={(e)=>this.handleChange(e.target.value)}
+                                            />
+                                            {questions[id].secondOption.text}
+                                        </label>
+                                        <button type="submit" className="primary-btn" disabled={this.state.value.length ===0}>Submit</button>
+                                    </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+                )}
+                { this.state.hasVoted && (
+                    <div>
+                        <div>
+                            <h3 className='result-header'>Asked By {users[questions[id].askedBy].name}</h3>
+                            <div className='result-div'>
+                                <h4 className='result-div-header'>Results</h4>
+                                <div className='result'>
+                                    <div className='result-image-section'>
+                                        <img
+                                            src={users[questions[id].askedBy].avatar}
+                                            alt={`${users[questions[id].askedBy].name}'s avatar`}
+                                        />
+                                    </div>
+                                    <div className='result-quest-section'>
+                                        <div className="firstOption">
+                                            <img
+                                                src={voted}
+                                                alt="voted-icon"
+                                                className='voted-icon'
+                                            />
+                                            <p>{questions[id].firstOption.text}</p>
+                                            <p><span>{`${firstPercentage}%`}</span> <span>{`${firstOptionVotes} of ${totalVotes} total votes`}</span></p>
+                                        </div>
+                                        <div className="secondOption">
+                                            <p>{questions[id].secondOption.text}</p>
+                                            <p><span>{`${secondPercentage}%`}</span> <span>{`${secondOptionVotes} of ${totalVotes} total votes`}</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                )}
+                
             </div>
         )
     }
@@ -116,6 +141,7 @@ class Question extends Component{
 
 function mapStateToProps({ questions, users, authedUser }, props){
     const { id } = props.match.params
+
 
     return{
         id,
